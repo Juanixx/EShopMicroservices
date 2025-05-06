2eed7a09a8adeb2207e1eb2360d11f013f0df392
Juan Calderon <juanixgh@gmail.com>
2025-05-06 00:06:20 -0600

    Update files and added endpoints and handlers

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Catalog.API.Products.GetProductById
 {
     public record GetProductByIdResponse(Product Product);
