0a76954f803ddd458fae9430e3edaba493dba73f
Juan Calderon <juanixgh@gmail.com>
2025-05-20 15:41:34 -0600

    Different improvements made

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -20,12 +20,10 @@ namespace Catalog.API.Products.UpdateProduct
         }
     }
 
-    internal class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    internal class UpdateProductCommandHandler(IDocumentSession session) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
     {
         public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
         {
-            logger.LogInformation("UpdateProductCommandHandler.Handle called with {@command}", command);
-
             var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
 
              if(product is null)
